bb699679fb94e45d57161bd1b98733828a4376dd
nitink3450 <nitinkanhurkar2016@gmail.com>
2026-06-03 18:17:45 +0530

    feat: implement react-router-dom configuration with product details route

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Navigate } from "react-router-dom";
 import App from "../App";
 import ProductDetails from "../pages/ProductDetails";
 
@@ -7,6 +7,10 @@ export const router = createBrowserRouter([
     path: "/",
     element: <App />,
     children: [
+      {
+        index: true,
+        element: <Navigate to="product/1" replace />,
+      },
       {
         path: "product/:id",
         element: <ProductDetails />,
